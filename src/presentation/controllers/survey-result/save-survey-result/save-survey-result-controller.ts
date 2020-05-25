6deca2bf4bc6ed76a7699eb145fc2511dd369298
Ianka Karúzia <iankakaruzia@hotmail.com>
2020-05-25 17:45:43 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,20 +1,33 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SaveSurveyResult } from './save-survey-result-controller-protocols'
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
+      const { accountId } = httpRequest
       const { answer } = httpRequest.body
       const survey = await this.loadSurveyById.loadById(surveyId)
+
       if (survey) {
         const answers = survey.answers.map(a => a.answer)
+
         if (!answers.includes(answer)) {
           return forbidden(new InvalidParamError('answer'))
         }
+
+        await this.saveSurveyResult.save({
+          accountId,
+          answer,
+          surveyId,
+          date: new Date()
+        })
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
