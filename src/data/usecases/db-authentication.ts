1bf31751ed322dc02fa14f4ec0a900ed60771e0e
Ianka Karuzia <iankakaruzia@gmail.com>
2020-12-29 23:00:26 -0300

    refactor: :recycle: add types to Authentication usecase

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -1,6 +1,4 @@
-
-import { Authentication, AuthenticationParams } from '@/domain/usecases'
-import { AuthenticationModel } from '@/domain/models'
+import { Authentication } from '@/domain/usecases'
 import { HashComparer, Encrypter, LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@/data/protocols'
 
 export class DbAuthentication implements Authentication {
@@ -11,7 +9,7 @@ export class DbAuthentication implements Authentication {
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
-  async auth (authenticationParams: AuthenticationParams): Promise<AuthenticationModel> {
+  async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
 
     if (account) {
