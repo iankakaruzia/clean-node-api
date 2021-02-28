4c015bba4b40ee34529956f80abd2e06616eea45
Ianka Karuzia <iankakaruzia@gmail.com>
2021-02-28 19:55:14 -0300

    test: ensure Login Query returns UnauthorizedError on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -49,5 +49,19 @@ describe('Login Graphql', () => {
       expect(res.data.login.accessToken).toBeTruthy()
       expect(res.data.login.name).toBe('Ianka')
     })
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const { query } = createTestClient({ apolloServer })
+
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'ianka@mail.com',
+          password: '12345'
+        }
+      })
+
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Unauthorized')
+    })
   })
 })
